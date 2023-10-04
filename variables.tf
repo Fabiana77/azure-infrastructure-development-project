89e012b916c7cc7a46d02e337b6b282e82744226
FabianaParra <70912175+Fabiana77@users.noreply.github.com>
2023-10-04 12:40:39 +0200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "count_vm" {
+  description = "Total number of Virtual Machines to create"
+  default = 3
+}
+
+variable "username" {
+  description = "The username for the Virtual Machine Resources"
+  default = "azure_user"
+}
+
+variable "password" {
+  description = "The password used for the Virtual Machine Resources"
+  default = "TestPassword123"
+}
