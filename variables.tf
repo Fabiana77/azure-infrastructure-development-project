49c40498284c548e0e7c0eeba5320278b38911da
FabianaParra <70912175+Fabiana77@users.noreply.github.com>
2023-10-04 20:16:08 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
 }
 
-variable "count_vm" {
+variable "countVM" {
   description = "Total number of Virtual Machines to create"
   default = 3
 }
